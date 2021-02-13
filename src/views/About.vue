c1b7fdf33f2ead5fb4fd1596c9eac918754a12ab
Jarunik <jens@hviid.ch>
2021-02-13 17:22:05 +0100

    header

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
       <li><a href="https://twitter.com/Jarunik" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://github.com/Jarunik" target="_blank" rel="noopener">Github</a></li>
     </ul>
-    <h1>Contribute</h1>
+    <h2>Contribute</h2>
     <p>You can contribute to expand this webpage on:</p>
     <ul>
       <li><a href="https://github.com/Jarunik/jarunikapp" target="_blank" rel="noopener">Github</a></li>
