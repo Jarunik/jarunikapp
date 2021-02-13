90f2e65b624c0fcef68eb7daa3c41a14d0782c07
Jarunik <jens@hviid.ch>
2021-02-13 16:33:27 +0100

    Added router

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About Jarunik</h1>
+    <ul>
+      <li><a href="https://twitter.com/Jarunik" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://github.com/Jarunik" target="_blank" rel="noopener">Github</a></li>
+    </ul>
+    <h1>Contribute</h1>
+    <p>You can contribute to expand this webpage on:</p>
+    <ul>
+      <li><a href="https://github.com/Jarunik/jarunikapp" target="_blank" rel="noopener">Github</a></li>
+    </ul>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Welcome to jarunik.com</h1>
     <p>
-      Iterative development. Please don't expect much in version 0.0.1
+      Iterative development. Please don't expect much in version 0.0.2
     </p>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://twitter.com/jarunik" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://explorer.ark.io/delegate-monitor" target="_blank" rel="noopener">Delegate Monitor</a></li>
     </ul>
     <h3>Ark Links</h3>
     <ul>
@@ -26,27 +27,11 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Home',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #0bbd27;
-}
+
 </style>
